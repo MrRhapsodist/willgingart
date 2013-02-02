eab53bd03c20ac5b7bc5163307b4bda1468b68aa
Alex Willging <rhapsodistchat@gmail.com>
2013-02-01 23:08:35 -0800

    Create prices.php

---
+++ prices.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h3>Special Rates for our NBC-Universal Clients</h3>";
+echo "<table class='prices' summary='Browse through our products and our prices to find the best deal'><tr><td class='prices'><strong>Frames</strong></td><td class='prices'>price</td><td class='prices'>price</td></tr><tr><td class='prices'><strong>Furniture</strong></td><td class='prices'>price</td><td class='prices'>price</td></tr><tr><td class='prices'><strong>Mirrors</strong></td><td class='prices'>price</td><td class='prices'>price</td></tr><tr><td class='prices'><strong>Artwork</strong></td><td class='prices'>price</td><td class='prices'>price</td></tr><tr><td class='prices'><strong>Consulting</strong></td><td class='prices'>price</td><td class='prices'>price</td></tr></table>";
+?>
