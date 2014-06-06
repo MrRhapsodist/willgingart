df67dd92fcb3070035683df833cea13a07659164
Alex Willging <rhapsodistchat@gmail.com>
2014-06-05 22:40:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var Request = false;
 if (window.XMLHttpRequest) {
-  Request = new XMLHttpRequest();
+	Request = new XMLHttpRequest();
 } else if (window.ActiveXObject) {
 	Request = new ActiveXObject("Microsoft.XMLHTTP");
 }
@@ -10,7 +10,7 @@ function ShowDiscount(php) {
 	if (Request) {
 		Request.open("POST", "prices.php", true);
 		Request.onreadystatechange = function() {
-			if(response == "UNIVERSALBEAGLE") {
+			if(response == "ARTFRIEND1") {
 				onclick = document.getElementById('trgtDiv').innerHTML=Request.responseText;
 			} else {
 				onclick = document.getElementById('trgtDiv').innerHTML="<h3>This code is not valid.  Normal prices may still apply.</h3>";
@@ -20,9 +20,22 @@ function ShowDiscount(php) {
 	}
 }
 function ShowPicture(url) {
-	newwindow=window.open(url, 'name', 'height=500, width=500');
+	var width = 500;
+	var height = 500;
+	var left = (screen.width - width)/2;
+	var top = (screen.height - height)/2;
+	var params = 'width='+width+', height='+height;
+	params += ', top='+top+', left='+left;
+	params += ', directories=no';
+	params += ', location=no';
+	params += ', menubar=no';
+	params += ', resizable=no';
+	params += ', scrollbars=no';
+	params += ', status=no';
+	params += ', toolbar=no';
+	newwindow=window.open(url, 'name', params);
 	if(window.focus) {
-		newwindow.focus()
+		newwindow.focus();
 	}
 	return false;
 }
